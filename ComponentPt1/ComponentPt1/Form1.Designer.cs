edb9d15f3a941589d669ab95c27bd162d7c18bce
Stephen Parkin <S.Parkin2824@student.leedsbeckett.ac.uk>
2023-10-24 16:14:37 +0100

    added shapes

--- ComponentPt1/ComponentPt1/Form1.Designer.cs
+++ ComponentPt1/ComponentPt1/Form1.Designer.cs
@@ -28,32 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.singleLine = new System.Windows.Forms.TextBox();
+            this.button_1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // singleLine
             // 
-            this.textBox1.Location = new System.Drawing.Point(7, 11);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(269, 22);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.singleLine.Location = new System.Drawing.Point(7, 11);
+            this.singleLine.Name = "singleLine";
+            this.singleLine.Size = new System.Drawing.Size(269, 22);
+            this.singleLine.TabIndex = 0;
+            this.singleLine.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // button1
+            // button_1
             // 
-            this.button1.Location = new System.Drawing.Point(282, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_1.Location = new System.Drawing.Point(282, 10);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(75, 23);
+            this.button_1.TabIndex = 1;
+            this.button_1.Text = "Run";
+            this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
             this.pictureBox1.Location = new System.Drawing.Point(7, 45);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(781, 393);
@@ -67,8 +68,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.singleLine);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -79,8 +80,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox singleLine;
+        private System.Windows.Forms.Button button_1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- ComponentPt1/ComponentPt1/Form1.cs
+++ ComponentPt1/ComponentPt1/Form1.cs
@@ -29,7 +29,53 @@ namespace ComponentPt1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
-        }
+
+            if (pictureBox1.Image != null)
+            {
+                Bitmap pictureBox = new Bitmap(pictureBox1.Height, pictureBox1.Width);
+            }
+
+            Graphics graphics = Graphics.FromImage(pictureBox);
+
+
+            String userInput = singleLine.Text;
+            String[] command = userInput.Split(' ');
+
+            graphics.Clear(Color.White);
+
+
+            if (command.Length > 1)
+            {
+                if ("circle".Equals(command[0]))
+                {
+                    graphics.FillEllipse(Brushes.DeepSkyBlue, 10, 10, float.Parse(command[1]), float.Parse(command[1]));
+                }
+
+                else if ("square".Equals(command[0]))
+                {
+                    graphics.FillRectangle(Brushes.Turquoise, 10, 10, float.Parse(command[1]), float.Parse(command[1]));
+                }
+
+                else if ("rectangle".Equals(command[0]))
+                {
+                    graphics.FillRectangle(Brushes.RoyalBlue, 10, 10, float.Parse(command[1]), float.Parse(command[2]));
+                }
+
+                else if ("moveTo".Equals(command[0]))
+                {
+                    throw new NotImplementedException();
+                }
+
+                else
+                {
+                    MessageBox.Show("Error Invlaid Shape");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Error, Invalid Shape");
+            }
+        }   
     }
 }
