c76b0ad3eb1148b69109fd9b193bbf24a2fc0961
Stephen Parkin <S.Parkin2824@student.leedsbeckett.ac.uk>
2023-11-10 11:34:39 +0000

    # converted most of the if else statments into switch cases # began moving where the grapics is setup

--- ComponentPt1/ComponentPt1/Form1.Designer.cs
+++ ComponentPt1/ComponentPt1/Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,10 +56,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Location = new System.Drawing.Point(388, 12);
+            this.pictureBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pictureBox1.Location = new System.Drawing.Point(368, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(405, 426);
+            this.pictureBox1.Size = new System.Drawing.Size(425, 426);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
@@ -76,30 +75,19 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(206, 411);
+            this.button2.Location = new System.Drawing.Point(287, 411);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Text = "Execute";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(287, 411);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.pictureBox1);
@@ -120,7 +108,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
--- ComponentPt1/ComponentPt1/Form1.cs
+++ ComponentPt1/ComponentPt1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,64 +25,131 @@ namespace ComponentPt1
 
         private void PictureBox1_Click(object sender, EventArgs e)
         {
+            Bitmap pictureBox = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = pictureBox;
 
+            var graphics = Graphics.FromImage(pictureBox);
         }
 
+        /// <summary>
+        /// Gets the user input for single line commands and check for an input
+        /// it does this by seeing if the textbox is empty or not, if it is then
+        /// it throws and exception, however if there is an input it will call the
+        /// shapeDrawring method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="ArgumentException"></exception>
         private void Button1_Click(object sender, EventArgs e)
+        {
+            String userInput = textBox1.Text;
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                throw new ArgumentException("Error, No Input");
+            }
+
+            else
+            {
+                ShapeDrawing(userInput);
+            }
+        }
+
+        /// <summary>
+        /// creates a loop that reads each line for the multi line command exectuion
+        /// it then calls the ShapeDrawing method to run the commands through so that
+        /// the input can be drawn.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            foreach (string singleLine in richTextBox1.Lines)
+            {
+                string userInput = singleLine;
+                ShapeDrawing(userInput);
+            }
+        }
+
+        /// <summary>
+        /// Gets the input and splits it into the individual components and
+        /// draws the corresponding shapes, it makes use of switch cases to
+        /// help prevent the duplication of code thus making it cleaner.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public void ShapeDrawing(string userInput)
         {
             Bitmap pictureBox = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             pictureBox1.Image = pictureBox;
 
             var graphics = Graphics.FromImage(pictureBox);
 
-            String userInput = textBox1.Text;
+            
             String[] command = userInput.Split(' ');
 
-            graphics.Clear(Color.White);
+            string shapeType = command[0].ToLower();
 
-            if (string.IsNullOrWhiteSpace(userInput))
+            switch (shapeType)
             {
-                throw new ArgumentException("Error, No Input");
+                case "clear":
+                    pictureBox1.Image = null;
+                    return;
+
+                case "refresh":
+                    graphics.Clear(Color.Transparent);
+                    return;
+
+                case "circle":
+                case "square":
+
+                    if (command.Length < 2)
+                    {
+                        throw new ArgumentException("Error Invalid Size");
+                    }
+                    float size = float.Parse(command[1]);
+
+                    if (command[0] == "circle")
+                    {
+                        graphics.FillEllipse(Brushes.DeepSkyBlue, 10, 10, size, size);
+                    }
+                    else
+                    {
+                        graphics.FillRectangle(Brushes.Turquoise, 10, 10, size, size);
+
+                    }
+                    break;
+
+                case "rectangle":
+                case "moveto":
+                case "drawto":
+                    if (command.Length < 3)
+                    {
+                        throw new InvalidOperationException("Error, Invalid Size");
+                    }
+                    switch (command[0])
+                    {
+                        case "rectanlge":
+                            graphics.FillRectangle(Brushes.RoyalBlue, 10, 10, float.Parse(command[1]), float.Parse(command[2]));
+                            break;
+                        
+                        case "drawto":
+                            graphics.DrawLine(Pens.Aqua, float.Parse(command[1]), float.Parse(command[2]), float.Parse(command[1]), float.Parse(command[2]));
+                            break;
+                        case "moveto":
+                            graphics.TranslateTransform(float.Parse(command[1]) - 10, float.Parse(command[2]) - 10);
+                            break;
+
+                    }
+                    break;
+
+                default:
+                    throw new ArgumentException($"{shapeType} is not supported.");
             }
-            
-            if (command.Length > 1)
-            {
-                if ("circle".Equals(command[0]))
-                {
-                    graphics.FillEllipse(Brushes.DeepSkyBlue, 10, 10, float.Parse(command[1]), float.Parse(command[1]));
-                }
-
-                else if ("square".Equals(command[0]))
-                {
-                    graphics.FillRectangle(Brushes.Turquoise, 10, 10, float.Parse(command[1]), float.Parse(command[1]));
-                }
-
-                else if ("rectangle".Equals(command[0]))
-                {
-                    graphics.FillRectangle(Brushes.RoyalBlue, 10, 10, float.Parse(command[1]), float.Parse(command[2]));
-                }
-            }
-
-            else
-            {
-                throw new ArgumentException("Error, Missing Size data");
-            }
-
-
-            pictureBox1.Refresh();
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
         {
 
         }
